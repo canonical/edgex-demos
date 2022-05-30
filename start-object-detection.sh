d8ae9e56d72aa05d42d3a626bd17f8ae42eb3e77
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2022-05-30 19:45:36 +0200

    Print http query resp errors

--- start-object-detection.sh
+++ start-object-detection.sh
@@ -10,7 +10,10 @@ CURL_RES=$(curl --show-error --silent --write-out "%{http_code}" \
 HTTP_CODE="${CURL_RES:${#CURL_RES}-3}"
 
 if [ $HTTP_CODE -ne 200 ] ; then
-    echo "Error on querying core command for the rtsp URL. exiting"
+    echo "Error on querying core command for the rtsp URL:"
+	# todo: check if body isn't empty  
+	BODY="${CURL_RES:0:${#CURL_RES}-3}"
+	echo $BODY
     exit 1
 fi
 
@@ -22,6 +25,7 @@ else
 fi
 
 RTSP_URL=$(echo $BODY | jq -r '.event.readings | .[] | .value')
+echo "RTSP URL of EdgeX Device Camera: $RTSP_URL"
 
 echo "Starting object detection demo"
 source ./data_processing/dl_streamer/bin/setupvars.sh
@@ -31,5 +35,5 @@ exec gst-launch-1.0 \
 	gvametaconvert format=json ! queue ! \
 	gvametapublish address=localhost:1883 method=mqtt topic=openvino/MQTT-test-device/prediction qos=true ! queue ! \
 	gvawatermark ! videoconvert ! fpsdisplaysink video-sink=autovideosink sync=false
-fi
+
 
